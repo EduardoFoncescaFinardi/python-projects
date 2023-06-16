8772ddb0d152d57fff776ed07b1d7369f1a71a4d
unknown <EduardoFoncescaFinardi>
2023-06-16 00:54:43 -0300

    feat:a

--- Lições de treino/treino_ferias/ex1.py
+++ Lições de treino/treino_ferias/ex1.py
@@ -1,10 +1,20 @@
 #Sistema de Gerenciamneto de Tarefas
 
-#parte 1: functionality that alllows the user to add tasks
+#part 1: functionality that alllows the user to add tasks
 
 def task_add(): 
   print("Want to add how many tasks?")
   tasks_amount = int(input("number os tasks:"))
   task_list = []
   for i in range(tasks_amount):
-    task_list.append(listadepecas[opcao - 1])
+    task = input("Describre task: ")
+    task_list.append(task)
+    i += 1
+  print(task_list)
+
+#part 2: 
+
+def main():
+  task_add()
+
+main()
