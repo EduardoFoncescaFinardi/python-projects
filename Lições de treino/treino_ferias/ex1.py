5563aa6ea6c7489be2652bb937c4429713f2b4d1
unknown <EduardoFoncescaFinardi>
2023-06-21 00:13:09 -0300

    training

--- Lições de treino/treino_ferias/ex1.py
+++ Lições de treino/treino_ferias/ex1.py
@@ -6,15 +6,31 @@ def task_add():
   print("Want to add how many tasks?")
   tasks_amount = int(input("number os tasks:"))
   task_list = []
-  for i in range(tasks_amount):
+  for i in  range(tasks_amount):
     task = input("Describre task: ")
     task_list.append(task)
     i += 1
-  print(task_list)
 
-#part 2: 
+  return task_list
+#part 2: functionality that alllows the user to edit tasks
+
+def task_edit(task_list):
+  print("Do you want to edit any task?")
+  confirmation = int(input("""1 - Yes 
+2 - No?
+"""))
+  
+  match confirmation: 
+    case 1:
+      print(task_list)
+      which_task = int(input("Which one?: "))
+    
+    case 2:
+      print
+
 
 def main():
-  task_add()
+  task_list = task_add()
+  task_edit(task_list)
 
 main()
